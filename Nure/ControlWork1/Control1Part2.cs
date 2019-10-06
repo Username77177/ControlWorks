82c91757022a357ae45649339e52715322ec8c9f
Username77177 <Username77177@gmail.com>
2019-10-06 19:35:02 +0300

    Add second t

--- Nure/ControlWork1/Control1Part2.cs
+++ Nure/ControlWork1/Control1Part2.cs
@@ -48,6 +48,22 @@ namespace ControlWork1
             }
             
             //Задание 2
+            double result = 1, result1 = 0;
+            Console.WriteLine("Введите значение n: ");
+            int it = Convert.ToInt32(Console.ReadLine());
+            while (it != 1)
+            {
+                //n!
+                result = result * it;
+                it -= 1;
+                //(-1)^n
+                double negativeornot = Math.Pow(-1, Convert.ToDouble(it));
+                //Fraction
+                result1 += negativeornot / result;
+                it -= 1;
+            }
+
+            Console.WriteLine("Сумма ряда ((-1)^n)/n! = " + (result1 -1) + ", при n = "+ it);
             
         }
     }
